5ec1a5d558cb7220c643d6f6ee94641ce59e8128
Sebastian <sebastian@Sebastians-MBP.fritz.box>
2021-09-16 14:13:15 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import Home from "./pages/Home";
+import Cart from  "./pages/Cart";
+import Login from "./pages/Login";
+import MyAccount from "./pages/MyAccount";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import PageNotFound from "./pages/PageNotFound"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" exact component={Cart} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/myAccount" exact component={MyAccount} />
+        <Route path="/productList" exact component={ProductList} />
+        <Route path="/register" exact component={Register} />
+        <Route path="*" exact component={PageNotFound}/>
+
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
